5d3b46b8a0da1c5392500375a95a3de9a8c48853
Bee Godwin <bee.godwin@bbc.co.uk>
2022-04-29 11:09:23 +0100

    add .babelrc to enable tests to work at all, update one failing test

--- test/game/my-scene.test.js
+++ test/game/my-scene.test.js
@@ -4,13 +4,13 @@ describe('MyScene', () => {
     let myScene;
     beforeEach(() => {
         myScene = new MyScene();
-        myScene.add = { image: jest.fn() };
+        myScene.add = { sprite: jest.fn() };
     });
 
     describe('create', () => {
         beforeEach(() => myScene.create());
-        test('loads an image', () => {
-            expect(myScene.add.image).toHaveBeenCalled();
+        test('adds a sprite', () => {
+            expect(myScene.add.sprite).toHaveBeenCalled();
         });
     });
 });
